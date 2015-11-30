bf20f1e6d2dc3545e7e42f78d1fcc2c5e8e081ae
Davide Gessa <gessadavide@gmail.com>
2015-11-30 19:36:20 +0100

    dapp update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(name='empty',
-	version='0.0.1',
+	version='0.1',
 	description='empty dapp library',
 	author='Empty Author',
 	author_email='empty@gmail.com',
